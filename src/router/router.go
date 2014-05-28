66860b5f6d7d99db49ea7c77fb894009f3e40c77
Qasim Zaidi <qasim@zaidi.me>
2014-05-28 12:11:19 +0530

    handle url scheme

--- src/router/router.go
+++ src/router/router.go
@@ -8,21 +8,49 @@ import (
   "github.com/nfnt/resize"
   "image/jpeg"
   "path"
+  "strings"
+  "strconv"
 )
 
 const (
-  assets = "/tmp"
-  cache = "./public"
+  Assets = "/tmp"
+  Cache = "./public"
+  Base = "/images/catalog/product/"
 )
+
+func getFilePathResQuality(url string) (path string, width, height, quality int) {
+  var res []string
+  path = strings.TrimPrefix(url,Base)
+  fields := strings.Split(path,"/")
+  length := len(fields)
+  path = Base + strings.Join(fields[0:3],"/") + "/" + fields[length-1]
+  switch (length) {
+    case 6:
+      quality,_ = strconv.Atoi(fields[4])
+      res = strings.Split(fields[3],"x")
+    case 5:
+      res = strings.Split(fields[3],"x")
+    case 4:
+    default:
+  }
+  if (res != nil) {
+    width,_ = strconv.Atoi(res[0])
+    height,_ = strconv.Atoi(res[1])
+  }
+  return
+}
 
 func Init() *http.ServeMux {
   mux := http.NewServeMux()
 
   mux.HandleFunc("/images/catalog/product/", func(w http.ResponseWriter, r* http.Request) {
-    filePath :=  assets + r.URL.Path
-    log.Println("You requested for ", filePath);
 
-    file, err := os.Open(filePath);
+    filePath,width,height,quality := getFilePathResQuality(r.URL.Path)
+
+    log.Println(filePath,width,height,quality)
+    log.Println("You requested for ", Assets + filePath);
+
+    file, err := os.Open(Assets + filePath);
     defer file.Close()
 
     if err != nil {
@@ -36,17 +64,20 @@ func Init() *http.ServeMux {
       return
     }
 
-    m := resize.Thumbnail(100, 100, img, resize.Lanczos3)
-    jpeg.Encode(w,m, &jpeg.Options{ Quality: 70 })
+    m := resize.Thumbnail(uint(width), uint(height), img, resize.Lanczos3)
+    q := jpeg.Options{ Quality: quality }
+    jpeg.Encode(w,m, &q)
 
     // cache the result as well, on disk
-    cachePath := cache + r.URL.Path
-    err = os.MkdirAll(path.Dir(cachePath),os.ModeDir)
-    if err != nil {
+    cachePath := Cache + r.URL.Path
+    err = os.MkdirAll(path.Dir(cachePath),os.ModeDir | 0777)
+    if err == nil {
       out, _ := os.Create(cachePath)
-      jpeg.Encode(out,m,nil)
+      jpeg.Encode(out,m,&q)
       out.Close()
       log.Println("cached into " + cachePath);
+    } else {
+      log.Println("cache fail ",err.Error())
     }
   })
 
