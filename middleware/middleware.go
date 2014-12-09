1bf47a52489e8d37b6e28bd0d45d74e803a158e3
Somesh <somesh.singh@paytm.com>
2014-12-09 18:10:16 +0530

    fix for no timeout. Moved ratelimit out side http call

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -8,18 +8,17 @@ import (
 // Ratelimit has to be pointed to github repo
 
 type RatelimitStruct struct {
-    rate int
+    limiter *ratelimit.Ratelimiter
 }
 
 // A struct that has a ServeHTTP method
 func Ratelimit(rate  int) *RatelimitStruct{
-    return &RatelimitStruct{rate}
+    rl, _  := ratelimit.NewRateLimiter(rate)
+    return &RatelimitStruct{rl}
 }
 
 func (r *RatelimitStruct) ServeHTTP(w http.ResponseWriter, req *http.Request, next http.HandlerFunc){
-    rl, _ := ratelimit.NewRateLimiter(r.rate)
-
-    if rl.Limit(){
+    if r.limiter.Limit(){
         http.Error(w, "Gateway Timeout", http.StatusGatewayTimeout )
     } else {
         next(w, req)
